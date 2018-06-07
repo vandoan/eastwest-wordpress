cf97258dc1c3905a3de1264e2e72602974a91be2
Van Doan <van.doan@mindbodyonline.com>
2018-06-06 23:14:24 -0700

    Goods - added plugin

---
+++ list-category-posts/nothumb.php
@@ -0,0 +1,123 @@
+<?php
+/*
+Plugin Name: List Category Posts - Template "Sofians Perfect Post List without Thumbs - http://sundari-webdesign.com/wordpress-the-quest-to-my-perfect-list-view-for-posts-events-and-articles"
+Plugin URI: http://picandocodigo.net/programacion/wordpress/list-category-posts-wordpress-plugin-english/
+Description: Template file for List Category Post Plugin for Wordpress which is used by plugin by argument template=value.php
+Version: 0.9
+Author: Radek Uldrych & Fernando Briano
+Author URI: http://picandocodigo.net http://radoviny.net
+*/
+
+/*
+Copyright 2009 Radek Uldrych (email : verex@centrum.cz)
+Copyright 2009-2015 Fernando Briano (http://picandocodigo.net)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or any
+later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+*/
+
+/**
+* The format for templates changed since version 0.17. Since this
+* code is included inside CatListDisplayer, $this refers to the
+* instance of CatListDisplayer that called this file.
+*/
+
+/* This is the string which will gather all the information.*/
+$lcp_display_output = '';
+
+//Show category link:
+$lcp_display_output .= $this->get_category_link('strong');
+
+//Show the conditional title:
+$lcp_display_output .= $this->get_conditional_title();
+
+//Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
+// (Sofian: We work with DIVs so this stays empty)
+$lcp_display_output .= '';
+
+/* Posts Loop
+ *
+ * The code here will be executed for every post in the category. As
+ * you can see, the different options are being called from functions
+ * on the $this variable which is a CatListDisplayer.
+ *
+ * CatListDisplayer has a function for each field we want to show. So
+ * you'll see get_excerpt, get_thumbnail, etc.  You can now pass an
+ * html tag as a parameter. This tag will sorround the info you want
+ * to display. You can also assign a specific CSS class to each field.
+*/
+global $post;
+while ( have_posts() ):
+  the_post();
+
+//Start a List Item for each post:
+// (Sofian: Here we open the DIV for each post.)
+    $lcp_display_output .= '<div class="postlist nothumb">';
+ 
+// (Sofian: Here we open an additional container DIV to wrap our content)     
+  $lcp_display_output .= '<div class="lcp-container">';
+
+  //Show the title and link to the post:
+  $lcp_display_output .= $this->get_post_title($post, 'h3'); 
+
+  //Show comments:
+  $lcp_display_output .= $this->get_comments($post);  
+
+  //Show date:
+  $lcp_display_output .= ' ' . $this->get_date($post);  
+
+  //Show date modified:
+  $lcp_display_output .= ' ' . $this->get_modified_date($post);
+
+  //Show author:
+  $lcp_display_output .= $this->get_author($post);   
+
+  //Custom fields:
+  $lcp_display_output .= $this->get_custom_fields($post);
+
+  /**
+   * Post content
+   */
+  $lcp_display_output .= $this->get_content($post, 'p', 'lcp_content');
+
+  /**
+   * Post excerpt
+   */
+  $lcp_display_output .= $this->get_excerpt($post, 'p', 'lcp_excerpt');
+  
+  // Get Posts "More" link:
+  // (Sofian: We add a class to right-align)
+  $lcp_display_output .= '<div class="morelink">' . $this->get_posts_morelink($post) . '</div>';
+
+  //Close li tag
+  // (Sofian: Closing our div)
+  $lcp_display_output .= '</div></div>';
+
+endwhile;
+
+// Close the wrapper I opened at the beginning:
+// (Sofian: We work with DIVs so this stays empty)
+$lcp_display_output .= '';
+
+// If there's a "more link", show it:
+$lcp_display_output .= $this->get_morelink();
+
+// Get category posts count
+$lcp_display_output .= $this->get_category_count();
+
+//Pagination
+$lcp_display_output .= $this->get_pagination();     
+
+$this->lcp_output = $lcp_display_output;
